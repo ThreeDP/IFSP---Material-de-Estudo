e78b054d9876bfca497225c6e92bbbafbd52fc61
ThreeDP <37334813+ThreeDP@users.noreply.github.com>
2019-09-27 18:22:30 -0300

    Update Calculo_tmp_exe.js

--- Linguagem de Programação/Javascript/Big-O/Calculo_tmp_exe.js
+++ Linguagem de Programação/Javascript/Big-O/Calculo_tmp_exe.js
@@ -1,11 +1,11 @@
- var calc = function(n) {
-    contador = 0;
+var calc = function(n) {
+    j = 1;
+    k = 1;
+    for (i = 1; n / 2; n) {
 
-    for(i = 1; n / 2; n) {
+        if (j + (n / 2) <= n) {
 
-        if(j + (n / 2) <= n) {
-
-            if(k <= n) {
+            if (k <= n) {
                 contador = contador++;
                 k = k * 2;
 
@@ -13,5 +13,9 @@
             j = j + 1;
         }
     }
-    window.print(contador);
- }
+    return contador
+}
+
+var contador = 0;
+var n = 1000;
+console.log(calc(n));
