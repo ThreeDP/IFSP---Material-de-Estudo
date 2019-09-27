0d9fe68b32ac62e56cd6f719d8faaf73e82571f2
ThreeDP <37334813+ThreeDP@users.noreply.github.com>
2019-09-26 22:30:35 -0300

    Add files via upload

---
+++ Linguagem de Programação/Javascript/Big-O/Calculo_tmp_exe.js
@@ -0,0 +1,17 @@
+ var calc = function(n) {
+    contador = 0;
+
+    for(i = 1; n / 2; n) {
+
+        if(j + (n / 2) <= n) {
+
+            if(k <= n) {
+                contador = contador++;
+                k = k * 2;
+
+            }
+            j = j + 1;
+        }
+    }
+    window.print(contador);
+ }
